f132a13d43766de5bedbf2a619164d0e1230c870
raianepadilha <PADILHA.RAIANE@ESCOLA.PR.GOV.BR>
2024-12-04 21:53:50 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Napoleão Bonaparte foi um líder militar e imperador francês, conhecido por suas conquistas durante as Guerras Napoleônicas.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é um número natural maior que 1 que só é divisível por 1 e ele mesmo.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Ciência',
+    'O que é a fotossíntese?',
+    'A fotossíntese é o processo pelo qual as plantas convertem luz solar, dióxido de carbono e água em glicose e oxigênio.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci, um dos maiores artistas do Renascimento.'
 )
